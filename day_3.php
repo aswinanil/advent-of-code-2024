acd961c429dd1f3fc7c8832165e4afb052b5a2e5
Aswin Anil <aswin@pigeonlab.com>
2025-01-01 13:01:35 +0530

    add solution for day 3, part 2

--- day_3.php
+++ day_3.php
@@ -2,15 +2,31 @@
 
 $filename = 'input/input_day_3.txt';
 /*$filename = 'input/sample_day_3.txt';*/
+//$filename = 'input/sample_day_3_2.txt';
 $contents = file($filename);
 
 $sum = 0;
 
+$useConditional = true;  // Toggle for part 1 answer
+
+$regex = $useConditional
+    ? "/(do\(\))|(don't\(\))|(mul\(\d{1,3},\d{1,3}\))/m"
+    : "/mul\(\d{1,3},\d{1,3}\)/m";
+
+$isDo = true;
+
 foreach($contents as $line) {
-    preg_match_all("/mul\(\d{1,3},\d{1,3}\)/m", $line, $matches);
+    preg_match_all($regex, $line, $matches);
 
     foreach($matches[0] as $match) {
-        $sum += multiply($match);
+        if ($match == "don't()") {
+            $isDo = false;
+        } else if ($match == "do()") {
+            $isDo = true;
+        } else if ($isDo) {
+            $sum += multiply($match);
+        } else {
+        }
     }
 }
 
