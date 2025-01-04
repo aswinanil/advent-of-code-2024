9e3ff57fe17fa32159778e73cf6e5b8a97ecc7ab
Aswin Anil <aswin@pigeonlab.com>
2025-01-04 10:24:57 +0530

    add solution for day 4, part 2

--- day_4.php
+++ day_4.php
@@ -16,13 +16,74 @@ $height = count($lines);
 $width = count($lines[0]);
 
 $pattern = "XMAS";
+$isPart1 = false;  // Toggle for part 1 answer
 
-foreach($lines as $y=>$line) {
-    foreach($line as $x=>$char) {
-        if ($char == "X") {
-            calcXmasCount($x, $y);
+if ($isPart1) {
+    foreach($lines as $y=>$line) {
+        foreach($line as $x=>$char) {
+            if ($char == "X") {
+                calcXmasCount($x, $y);
+            }
         }
     }
+} else {
+    foreach($lines as $y=>$line) {
+        foreach($line as $x=>$char) {
+            if ($char == "A") {
+                calcTwoMasCount($x, $y);
+            }
+        }
+    }
+}
+
+function calcTwoMasCount($x, $y) {
+    global $height;
+    global $width;
+    global $count;
+
+    if (!isLeftMas($x, $y)) {
+        return;
+    }
+
+    if (!isRightMas($x, $y)) {
+        return;
+    }
+
+    $count++;
+}
+
+function isLeftMas($x, $y) {
+    global $lines;
+
+    if (
+        isOutOfBounds($x-1, $y+1) ||
+        isOutOfBounds($x+1, $y-1)
+    ) {
+        return false;
+    }
+
+    $topLeft = $lines[$y+1][$x-1];
+    $btmRight = $lines[$y-1][$x+1];
+
+    return $topLeft == "M" && $btmRight == "S"
+        || $topLeft == "S" && $btmRight == "M";
+}
+
+function isRightMas($x, $y) {
+    global $lines;
+
+    if (
+        isOutOfBounds($x+1, $y+1) ||
+        isOutOfBounds($x-1, $y-1)
+    ) {
+        return false;
+    }
+
+    $topRight = $lines[$y+1][$x+1];
+    $btmLeft = $lines[$y-1][$x-1];
+
+    return $topRight == "M" && $btmLeft == "S"
+        || $topRight == "S" && $btmLeft == "M";
 }
 
 function calcXmasCount($x, $y) {
